7d73e91d587baba24d9d96c1dda7065e72d8f78f
josip-k <d3hum4niz3d@gmail.com>
2021-02-03 09:53:20 +0100

    RMX199X: build initial graphics packages
    Change-Id: I925f54d339ef68c59e33d7585c4cfd0f5739b028

---
+++ device/product_packages.mk
@@ -0,0 +1,13 @@
+### GRAPHICS
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl:64 \
+    android.hardware.graphics.allocator@2.0-service 
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1
+
+# Display
+PRODUCT_PACKAGES += \
+    gralloc.sm6150 \
+    memtrack.sm6150
